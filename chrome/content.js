0cf9597fbfe2696dbb81c47dc9b9089a34124823
han4wluc <han4wluc@gmail.com>
2016-06-11 21:37:46 +0800

    improve jquery replace

--- chrome/content.js
+++ chrome/content.js
@@ -2,16 +2,8 @@
 * @Author: lu
 * @Date:   2016-06-11 14:09:30
 * @Last Modified by:   han4wluc
-* @Last Modified time: 2016-06-11 18:39:26
+* @Last Modified time: 2016-06-11 21:37:14
 */
-
-// var tokenize = function(inputText){
-  // if(inputText.includes('教育')){
-  //   var newHtml = replace(node.nodeValue, '教育');
-  // }
-// }
-
-// console.log($('body').text());
 
 var ProofReader = {
   proofread: function(hashes){
@@ -63,28 +55,23 @@ var ProofReader = {
 
 var entireContent = $('body').text();
 
-var getWrongWords = function(input){
-  return ['教育', '购了', '中自'];
-}
+// var getWrongWords = function(input){
+//   return ['教育', '购了', '中自'];
+// }
 
-var replace = function(inputText, replaceWord){
-  var regex = new RegExp(replaceWord, 'g');
-  var replaceResult = '<span class="error-underline">' + replaceWord + '<span class="tooltiptext">Grammar Error</span>' + '</span>';
-  // var replaceResult = '<span class="error-underline">' + replaceWord + '</span>';
-  return inputText.replace(replaceWord, replaceResult);
-}
+// var replace = function(inputText, replaceWord){
+//   var regex = new RegExp(replaceWord, 'g');
+//   // var replaceResult = '<span class="error-underline">' + replaceWord + '<span class="tooltiptext">Grammar Error</span>' + '</span>';
+//   var replaceResult = '<span class="error-underline">' + replaceWord + '</span>';
+//   return inputText.replace(replaceWord, replaceResult);
+// }
 
 var paragraphs = {};
 var hashes = {};
 
 function recursiveReplace(node) {
 
-  // $(node).addClass('gfsd');
-  // console.log(node);
-
   if (node.nodeType == 3 && node.nodeValue.trim().length > 2) { // text node
-
-    // console.log(node);
 
     var paragraph = node.nodeValue.trim();
 
@@ -92,21 +79,10 @@ function recursiveReplace(node) {
 
     paragraphs[hash] = paragraph;
     hashes[hash] = paragraph;
-    // hashes.push({
-    //   hash: hash,
-    //   paragraph: paragraph,
-    // });
+
 
     $(node).parent().addClass(hash);
 
-    // var wrongWords = getWrongWords(paragraph);
-
-    // var newHtml = paragraph;
-    // wrongWords.forEach(function(value){
-    //   newHtml = replace(newHtml, value);
-    // })
-
-    // $(node).replaceWith(newHtml);
 
   } else if (node.nodeType == 1) { // element
     $(node).contents().each(function () {
@@ -142,48 +118,26 @@ var joinWordsAndPunc = function(words, mixedPunctuations){
     }
   });
 
-  console.log('combined', uniqueCombinedObjectes);
+  // console.log('combined', uniqueCombinedObjectes);
   return uniqueCombinedObjectes;
 }
 
-// var wrongWordsObj = expected.words;
 var wrongWordsObj = joinWordsAndPunc(expected.words, expected.mixedPunctuation);
-  console.log('wrongWordsObj', wrongWordsObj);
 
 wrongWordsObj.forEach(function(wordObj){
+
   var node = $('.'+wordObj.hash);
-  var paragraph = node.text();
 
-
-  var newHtml = paragraph;
-  wordObj.words.forEach(function(wordWithPos){
-    //todo use position
-    newHtml = replace(newHtml, wordWithPos.word);
+  var wrongWords = wordObj.words.map(function(word){
+    return word.word;
   });
- 
-  // ['国外', '教育', '这个', '媒体'].forEach(function(word){
-  //   //todo use position
-  //   newHtml = replace(newHtml, word);
-  // });
 
-
-  // console.log(newHtml);
-  node.html(newHtml);
+  $(node).contents().each(function(){
+    if(this.nodeType === 3){
+      var regex = new RegExp('(' +  wrongWords.join('|') + ')', 'g');
+      var oldHtml = $(this).text();
+      var newHtml = oldHtml.replace(regex, '<span class="error-underline">$1<span class="tooltiptext">Grammar Error</span></span>');
+      $(this).replaceWith(newHtml);
+    }
+  })
 })
-
-// ['国外', '教育'].forEach(function(word){
-//   //todo use position
-//   newHtml = replace(newHtml, word);
-// });
-
-// setTimeout(function(){
-//   hashes.forEach((function(hash){
-//     var node = $('.'+hash);
-//     var paragraph = node.text();
-//     var newHtml = replace(paragraph, '教育');
-//     node.html(newHtml);
-//   }))
-// },100)
-
-// console.log('paragraphs', paragraphs);
-
